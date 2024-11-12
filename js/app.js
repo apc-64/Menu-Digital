b1bc715bc4c3cd693f7427d3ed626e559a9c2270
apc64 <adrianodev64@gmail.com>
2024-11-12 16:35:24 -0400

    badge carrinho

--- js/app.js
+++ js/app.js
@@ -4,6 +4,8 @@ $(document).ready(function () {
 })
 
 var cardapio = {};
+var MEU_CARRINHO = [];
+
 
 cardapio.eventos = {
 
@@ -73,6 +75,67 @@ cardapio.metodos = {
         
         $("#qntd-carrinho-"+id).text(qntdAtual + 1);
     },
+
+    adicionarCarrinho: (id) => {
+        let qntdAtual = parseInt($("#qntd-carrinho-"+id).text());
+        
+
+        //grep cria um outro vetor, com elementos que atendam suas condições, sem alterar o original. RETORNA OBJETOS
+        //filter é o mesmo, com objetos e callback. NÃO RETORNA OBJETOS
+        if(qntdAtual > 0){
+            //obtem a categoria
+            var categoria = $(".container-menu a.active").attr('id').split('menu-')[1];
+
+            //obtem todos os itens da categoria atuak
+            let filtro = MENU[categoria];
+
+            //pega o item a ser adicionado
+            let item = $.grep(filtro, (e, i) => {return e.id == id});
+
+
+            if(item.lenght > 0){
+
+                //validar se este item já existe
+                let existe = $.grep(MEU_CARRINHO, (e, i) => {return e.id == id})
+    
+                if(existe.lenght > 0){
+                    let objIndex = MEU_CARRINHO.findIndex((obj => obj.id == id));
+                    MEU_CARRINHO[objIndex].qntd = MEU_CARRINHO[objIndex].qntd + qntdAtual;
+                }
+                else{
+                    item[0].qntd = qntdAtual;
+                    MEU_CARRINHO.push(item[0]);
+                }
+    
+                cardapio.metodos.atualizarBadgeTotal();
+    
+            }
+        }
+
+        
+        
+
+
+    },
+
+    atualizarBadgeTotal: () => {
+        var total = 0;
+
+        $.each(MEU_CARRINHO, (i, e) =>{
+            total += e.qntd
+        })
+
+        if(total > 0){
+            $(".botao-carrinho").removeClass('hidden');
+            $(".container-total-carrinho").removeClass('hidden');
+        }else{
+            $(".botao-carrinho").addClass('hidden');
+            $(".container-total-carrinho").addClass('hidden');
+        }
+
+        $(".badge-total-carrinho").html(total);
+
+    }
 }
 
 cardapio.templates = {
@@ -93,7 +156,7 @@ cardapio.templates = {
             <span class="btn-menos" onclick="cardapio.metodos.diminuirQuantidade('\${id}')"><i class="fas fa-minus"></i></span>
             <span class="btn-numero-itens" id="qntd-carrinho-\${id}">0</span>
             <span class="btn-mais" onclick="cardapio.metodos.aumentarQuantidade('\${id}')"><i class="fas fa-plus"></i></span>
-            <span class="btn btn-add"><i class="fa fa-shopping-bag"></i></span>
+            <span class="btn btn-add" onclick="cardapio.metodos.adicionarCarrinho('\${id}')"><i class="fa fa-shopping-bag"></i></span>
         </div>
         </div>
     </div>`
